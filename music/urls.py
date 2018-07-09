5334bab0276dd078342964ddd6505d86287e20a7
Tasnuvaleeya <tasnuva2606@gmail.com>
2018-07-09 12:08:32 +0600

    added view

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, detail
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^(?P<album_id>[0-9]+)/$', detail, name="detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album, Song
 
 
 def index(request):
-    return HttpResponse("This Is Music Homepage")
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+
+
